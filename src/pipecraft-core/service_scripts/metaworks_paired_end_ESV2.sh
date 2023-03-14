b2485e10bedaff4fe5c457843d4437903a3286bb
anslan <sten.anslan@gmail.com>
2023-03-14 17:46:13 +0200

    mw_test

--- src/pipecraft-core/service_scripts/metaworks_paired_end_ESV2.sh
+++ src/pipecraft-core/service_scripts/metaworks_paired_end_ESV2.sh
@@ -3,10 +3,21 @@
 eval "$(conda shell.bash hook)"
 conda activate MetaWorks_v1.11.2
 
-cd /MetaWorks1.11.2
-snakemake --jobs 2 --snakefile snakefile_ESV --configfile /input/config_testing_COI_data.yaml
+set -e 
 
-echo "workingDir=metaworks_output"
+cd /MetaWorks1.11.2
+
+#specify output dir in the config file
+#mkdir -p /input/docmw2
+
+sed -e 's/dir:.*/dir: "\/input\/docmw2"/' config_testing_COI_data.yaml > config.yaml
+
+#specify database
+sed -i 's/\/path\/to\/mydata_trained\/rRNAClassifier.properties/\/extraFiles2\/rRNAClassifier.properties/' config.yaml 
+#run snakemake
+snakemake --jobs 2 --snakefile snakefile_ESV --configfile config.yaml
+
+#echo "workingDir=metaworks_output"
 echo "fileFormat=fastq"
 echo "dataFormat=demultiplexed"
 echo "readType=paired_end"
