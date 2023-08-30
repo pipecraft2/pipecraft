a93757ee5e0400a5dbec601050e6926ac9389e52
anslan <sten.anslan@gmail.com>
2023-08-30 15:23:14 +0300

    fileFormat fix

--- src/pipecraft-core/service_scripts/quality_filtering_paired_end_dada2_mixed.sh
+++ src/pipecraft-core/service_scripts/quality_filtering_paired_end_dada2_mixed.sh
@@ -22,18 +22,19 @@ readType=${readType}
 dataFormat=${dataFormat}
 workingDir=${workingDir}
 
-#load variables
-read_R1=${read_R1}
-read_R2=${read_R2}
-maxEE=${maxEE}
-maxN=${maxN}
-truncQ=${truncQ}
-truncLen_R1=${truncLen}
-truncLen_R2=${truncLen_R2}
-minLen=${minLen}
-maxLen=${maxLen}
-minQ=${minQ}
-matchIDs=${matchIDs}
+### variables
+# read_R1     = identifyer string that is common for all R1 reads.
+# read_R2     = identifyer string that is common for all R2 reads.
+# maxEE       = discard sequences with more than the specified number of expected errors
+# maxN        = discard sequences with more than the specified number of Ns (ambiguous bases)
+# truncQ      = truncate reads at the first instance of a quality score less than or equal to truncQ
+# truncLen    = truncate reads after truncLen bases (applies to R1 reads when working with paired-end data)
+# truncLen_R2 = truncate R2 reads after truncLen bases
+# minLen      = remove reads with length less than minLen. minLen is enforced after all other trimming and truncation
+# maxLen      = remove reads with length greater than maxLen. maxLen is enforced on the raw reads
+# minQ        = after truncation, reads contain a quality score below minQ will be discarded
+# matchIDs    = If TRUE, then double-checking (with seqkit pair) that only paired reads that share ids are outputted
+                        #WORK WITH SEQKIT for matchIDs = TRUE, because sometimes DADA2 CANNOT automatically identify paired-end headers
 
 #Source for functions
 source /scripts/submodules/framework.functions.sh
@@ -47,10 +48,10 @@ start=$(date +%s)
 if [[ -d "/input/primersCut_out/fwd_orient" ]] && [[ -d "/input/primersCut_out/rev_orient" ]]; then
     :
 else
-       printf '%s\n' "ERROR]: input folders primersCut_out/fwd_orient and/or primersCut_out/rev_orient not found!
-       (CUT PRIMERS is mandatory when amplicons are MIXED orient)
+    printf '%s\n' "ERROR]: input folders primersCut_out/fwd_orient and/or primersCut_out/rev_orient not found!
+    (CUT PRIMERS is mandatory when amplicons are MIXED orient)
     >Quitting" >&2
-        end_process
+    end_process
 fi
 
 #define quality_filtering_paired_end_dada2 function
@@ -81,7 +82,6 @@ function quality_filtering_paired_end_dada2 () {
             :
         else
             printf '%s\n' "ERROR]: 'read R1/R2' identifiers are incorrectly specified.
-            Check also 'samp ID' setting.
             >Quitting" >&2
             end_process
         fi
@@ -90,7 +90,7 @@ function quality_filtering_paired_end_dada2 () {
     ### Process samples with dada2 filterAndTrim function in R
     printf "# Running DADA2 filterAndTrim \n"
     Rlog=$(Rscript /scripts/submodules/dada2_PE_filterAndTrim.R 2>&1)
-    echo $Rlog > $output_dir/filterAndTrim.log 
+    echo $Rlog > $output_dir/dada2_PE_filterAndTrim.log 
     wait
     #format R-log file
     sed -i "s/;; /\n/g" $output_dir/dada2_PE_filterAndTrim.log 
@@ -101,17 +101,22 @@ function quality_filtering_paired_end_dada2 () {
             #Read in R1 and R2 file names; without extension
             samp_name=$(basename $LINE | awk -F\\${read_R1} '{print$1}')
             #If outputs are not empty, then synchronize R1 and R2
-            if [[ -s $output_dir/$samp_name\_R1.$extension ]]; then
-                if [[ -s $output_dir/$samp_name\_R2.$extension ]]; then
+
+            echo "test $output_dir/$samp_name\_R1.$fileFormat"
+            echo "pwd = "
+            echo "$PWD"
+
+            if [[ -s $output_dir/$samp_name\_R1.$fileFormat ]]; then
+                if [[ -s $output_dir/$samp_name\_R2.$fileFormat ]]; then
                     printf "\nSynchronizing $samp_name R1 and R2 reads\n"
                     cd $output_dir
-                    checkerror=$(seqkit pair -1 $samp_name\_R1.$extension -2 $samp_name\_R2.$extension 2>&1)
+                    checkerror=$(seqkit pair -1 $samp_name\_R1.$fileFormat -2 $samp_name\_R2.$fileFormat 2>&1)
                     check_app_error
 
-                    rm $samp_name\_R1.$extension
-                    rm $samp_name\_R2.$extension
-                    mv $samp_name\_R1.paired.$extension $samp_name\_R1.$extension
-                    mv $samp_name\_R2.paired.$extension $samp_name\_R2.$extension
+                    rm $samp_name\_R1.$fileFormat
+                    rm $samp_name\_R2.$fileFormat
+                    mv $samp_name\_R1.paired.$fileFormat $samp_name\_R1.$fileFormat
+                    mv $samp_name\_R2.paired.$fileFormat $samp_name\_R2.$fileFormat
                     cd ..
                 fi
             else
@@ -127,11 +132,11 @@ function quality_filtering_paired_end_dada2 () {
     fi
 
     ### end pipe if no outputs were generated
-    outfile_check=$(ls $output_dir/*.$extension 2>/dev/null | wc -l)
+    outfile_check=$(ls $output_dir/*.$fileFormat 2>/dev/null | wc -l)
     if [[ $outfile_check != 0 ]]; then 
         :
     else 
-        printf '%s\n' "ERROR]: no output files generated after quality filtering ($output_dir). Adjust settings or check sample identifier 'samp ID' so that all sample names would be unique.
+        printf '%s\n' "ERROR]: no output files generated after quality filtering ($output_dir). Adjust settings or check sample identifier 'read_R1/R2' so that all sample names would be unique.
         >Quitting" >&2
         end_process
     fi
@@ -156,12 +161,12 @@ for folder in /input/primersCut_out/fwd_orient /input/primersCut_out/rev_orient;
     Detected MIXED orient sequences as input data; note that there were independent runs for fwd_orient and rev_orient inputs.
 
     Files in 'qualFiltered_out':
-    # *.$extension              = quality filtered sequences per sample.
+    # *.$fileFormat              = quality filtered sequences per sample.
     # seq_count_summary.txt     = summary of sequence counts per sample.
     # *.rds                     = R objects for dada2.
 
     Core command -> 
-    filterAndTrim(inputR1, outputR1, inputR2, outputR2, maxN = $maxN, maxEE = c($maxEE, $maxEE), truncQ = $truncQ, truncLen= c($truncLen_R1, $truncLen_R2), maxLen = $maxLen, minLen = $minLen, minQ=$minQ, rm.phix = TRUE)
+    filterAndTrim(inputR1, outputR1, inputR2, outputR2, maxN = $maxN, maxEE = c($maxEE, $maxEE), truncQ = $truncQ, truncLen= c($truncLen, $truncLen_R2), maxLen = $maxLen, minLen = $minLen, minQ=$minQ, rm.phix = TRUE)
 
     Total run time was $runtime sec for $output_dir.
     ##################################################################
@@ -183,6 +188,6 @@ printf "Total time: $runtime sec.\n "
 #variables for all services
 echo "#variables for all services: "
 echo "workingDir=$output_dir"
-echo "fileFormat=$extension"
+echo "fileFormat=$fileFormat"
 echo "readType=paired_end"
 
