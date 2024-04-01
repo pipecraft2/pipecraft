64a4119abbe538f6050c68133e0de326ef275792
anslan <sten.anslan@gmail.com>
2024-04-01 13:57:34 +0300

    typo fix

--- src/pipecraft-core/service_scripts/submodules/dada2_chimeraFilt_wf.R
+++ src/pipecraft-core/service_scripts/submodules/dada2_chimeraFilt_wf.R
@@ -53,7 +53,7 @@ for (i in 1:nrow(ASV_tab.nochim)){
 ASV_counts = data.frame(no_of_ASVs_list, check.names = FALSE, row.names = "")
 colnames(ASV_counts) = sample_names
 seq_count <- cbind(rowSums(ASV_tab), rowSums(ASV_tab.nochim), t(ASV_counts))
-colnames(seq_count) <- c("input(merged)", "chimeraFiltered", "no.of ASVs")
+colnames(seq_count) <- c("input(denoised)", "chimeraFiltered", "no.of ASVs")
 rownames(seq_count) <- sample_names
 write.csv(seq_count, file.path(path_results, "seq_count_summary.csv"), row.names = TRUE, quote = FALSE)
 
