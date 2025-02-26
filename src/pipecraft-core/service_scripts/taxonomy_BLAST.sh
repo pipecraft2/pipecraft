b7554fce8d92375e9d2fe68fbc856663611cdc15
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2025-02-26 13:46:00 +0000

    bugs removed

--- src/pipecraft-core/service_scripts/taxonomy_BLAST.sh
+++ src/pipecraft-core/service_scripts/taxonomy_BLAST.sh
@@ -5,13 +5,13 @@
 #If database = fasta file, then makeblastdb with BLAST+, otherwise run BLAST
 #Outputs: BLAST_1st_best_hit.txt, BLAST_10_best_hits.txt
 
-################################################
+##########################################################
 ###Third-party applications:
 # BLAST+
 # python3 with biopython module
 # seqkit
 # gawk
-################################################
+##########################################################
 
 #load variables
 #database [# here, a possibility for multiple databases to be added: database=$"-db $db1 $db2 $db3 $db4 $db5"]
@@ -19,12 +19,6 @@ regex='[^/]*$'
 db1_temp=$(echo $database_file | grep -oP "$regex")
 db1=$(printf "/extraFiles/$db1_temp")
 echo "db1 = $db1"
-fasta_file=$(echo $fasta_file | grep -oP "$regex")
-fasta_file=$(printf "/extraFiles2/$fasta_file")
-echo "fasta_file = $fasta_file"
-# Extract the base filename from fasta_file
-IN=$(basename "$fasta_file")
-echo "input = $IN"
 #mandatory options
 task=$"-task ${task}" # list: blastn, megablast
 strands=$"-strand ${strands}" #list: both, plus
@@ -50,7 +44,6 @@ output_dir=$"/input/taxonomy_out"
 #############################
 ### Start of the workflow ###
 #############################
-start_time=$(date)
 start=$(date +%s)
 ### Check if files with specified extension exist in the dir
 first_file_check
@@ -60,6 +53,11 @@ prepare_SE_env
 check_gz_zip_SE
 ### Check input formats (fasta supported)
 check_extension_fasta
+### Select last fasta file in the folder as input for BLAST
+for file in *.$fileFormat; do
+    IN=$(echo $file)
+done
+echo "input = $IN"
 
 ### Check and assign BLAST database
 d1=$(echo $db1 | awk 'BEGIN{FS=OFS="."}{print $NF}') #get the extension
@@ -67,24 +65,22 @@ d1=$(echo $db1 | awk 'BEGIN{FS=OFS="."}{print $NF}') #get the extension
 db_dir=$(dirname $db1)
 check_db_presence=$(ls -1 $db_dir/*.nhr 2>/dev/null | wc -l)
 if (( $check_db_presence != 0 )); then
-	if [[ $d1 == "fasta" ]] || [[ $d1 == "fa" ]] || [[ $d1 == "fas" ]] || [[ $d1 == "fna" ]] || [[ $d1 == "ffn" ]]; then
-		database=$"-db $db1"
-	elif [[ $d1 == "ndb" ]] || [[ $d1 == "nhr" ]] || [[ $d1 == "nin" ]] || [[ $d1 == "not" ]] || [[ $d1 == "nsq" ]] || [[ $d1 == "ntf" ]] || [[ $d1 == "nto" ]]; then
-		db1=$(echo $db1 | awk 'BEGIN{FS=OFS="."}NF{NF-=1};1')
-		database=$"-db $db1"
-	fi
+    if [[ $d1 == "fasta" ]] || [[ $d1 == "fa" ]] || [[ $d1 == "fas" ]] || [[ $d1 == "fna" ]] || [[ $d1 == "ffn" ]]; then
+        database=$"-db $db1"
+    elif [[ $d1 == "ndb" ]] || [[ $d1 == "nhr" ]] || [[ $d1 == "nin" ]] || [[ $d1 == "not" ]] || [[ $d1 == "nsq" ]] || [[ $d1 == "ntf" ]] || [[ $d1 == "nto" ]]; then
+        db1=$(echo $db1 | awk 'BEGIN{FS=OFS="."}NF{NF-=1};1')
+        database=$"-db $db1"
+    fi
 elif [[ $d1 == "fasta" ]] || [[ $d1 == "fa" ]] || [[ $d1 == "fas" ]] || [[ $d1 == "fna" ]] || [[ $d1 == "ffn" ]]; then
-		printf '%s\n' "Note: converting fasta formatted database for BLAST"
-		makeblastdb -in $db1 -input_type fasta -dbtype nucl
-		database=$"-db $db1"
+        printf '%s\n' "Note: converting fasta formatted database for BLAST"
+        makeblastdb -in $db1 -input_type fasta -dbtype nucl
+        database=$"-db $db1"
 fi
 
 ## Perform taxonomy annotation
 printf '%s\n' "Running BLAST"
-echo "input = $fasta_file"
-
 checkerror=$(blastn \
--query $fasta_file \
+-query $IN \
 $strands \
 $cores \
 $database \
@@ -101,26 +97,23 @@ $gapextend \
 -outfmt "6 delim=+ qseqid stitle qlen slen qstart qend sstart send evalue length nident mismatch gapopen gaps sstrand qcovs pident" 2>&1)
 check_app_error
 
-# Compute similarity score and append as new column to BLAST output file
-awk -F'+' 'BEGIN {OFS="+"} {sim = $17 * ($10 / $3); print $0, sim}' "$output_dir/10BestHits.txt" > "$output_dir/10BestHits.tmp" && mv "$output_dir/10BestHits.tmp" "$output_dir/10BestHits.txt"
-
 ### Parse 10BestHits ### QUERY SEQUENCE HEADERS MUST BE UNIQE! 
 cd $output_dir
 #get only first occurrence of a duplicate row (1st hit)
 mkdir -p tempdir
-awk 'BEGIN{FS="+"} !seen[$1]++' 10BestHits.txt > tempdir/1.temphit #sep = +
+awk 'BEGIN{FS="+"}''!seen[$1]++' 10BestHits.txt > tempdir/1.temphit #sep = +
 #check which seqs got a hit
 gawk 'BEGIN{FS="+"}{print $1}' < tempdir/1.temphit | uniq > tempdir/gothits.names
 #add no_hits flag
 seqkit seq -n ../$IN > tempdir/$IN.names
 grep -v -w -F -f tempdir/gothits.names tempdir/$IN.names | sed -e 's/$/\tNo_significant_similarity_found/' >> tempdir/1.temphit
 #add header
-sed -e '1 i\qseqid+1st_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score' tempdir/1.temphit > tempdir/BLAST_1st_hit.txt 
+sed -e '1 i\qseqid+1st_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident' tempdir/1.temphit > tempdir/BLAST_1st_hit.txt 
 
 ###10hits
 #do the same for next 2-10 hits
 for i in {2..10}; do 
-    awk -v i="$i" 'BEGIN{FS="+"} ++seen[$1]==i' 10BestHits.txt > tempdir/$i.temphit
+    awk -v i="$i" 'BEGIN{FS="+"}''++seen[$1]==i' 10BestHits.txt > tempdir/$i.temphit
     gawk 'BEGIN{FS="+"}{print $1}' < tempdir/$i.temphit | uniq > tempdir/gothits.names
     grep -v -w -F -f tempdir/gothits.names tempdir/$IN.names | sed -e 's/$/\tNo_BLAST_hit/' >> tempdir/$i.temphit && rm tempdir/gothits.names
 done
@@ -134,7 +127,7 @@ rm tempdir/*.temp
 #format 10 hits
 sed -i 's/No_significant_similarity_found.*/No_significant_similarity_found/' BLAST_10_best_hits.txt
 sed -i 's/No_BLAST_hit.*/No_BLAST_hit/' BLAST_10_best_hits.txt
-sed -i '1 i\qseqid+1st_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+2nd_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+3rd_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+4th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+5th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+6th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+7th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+8th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+9th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score+qseqid+10th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score' BLAST_10_best_hits.txt
+sed -i '1 i\qseqid+1st_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+2nd_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+3rd_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+4th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+5th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+6th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+7th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+8th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+9th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+qseqid+10th_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident' BLAST_10_best_hits.txt
 
 ##### BLAST 1st hit with query SEQ ######
 #fasta to oneline
@@ -146,11 +139,39 @@ sed -i '/^$/d' BLAST_1st_best_hit.temp
 sort -k 1 --field-separator=\t tempdir/$IN.oneline | sed -e 's/^>//' | sed -e 's/\r//' > tempdir/seqs.txt
 #merge seqs and 1st hit
 paste tempdir/seqs.txt BLAST_1st_best_hit.temp > BLAST_1st_best_hit.txt && rm BLAST_1st_best_hit.temp
-sed -i '1 i\qseqid+query_seq+qseqid+1st_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident+sim_score' BLAST_1st_best_hit.txt
+sed -i '1 i\qseqid+query_seq+qseqid+1st_hit+qlen+slen+qstart+qend+sstart+send+evalue+length+nident+mismatch+gapopen+gaps+sstrand+qcovs+pident' BLAST_1st_best_hit.txt
 
 mv 10BestHits.txt tempdir/
 sed -i 's/\t/+/g' BLAST_1st_best_hit.txt
 sed -i 's/\t/+/g' BLAST_10_best_hits.txt
+
+# Add sim_score column to BLAST_1st_best_hit.txt
+awk -F'+' 'BEGIN{OFS="+"}
+NR==1 {print $0, "sim_score"; next}
+{
+    sim = $19 * ($12 / $5);
+    print $0, sim
+}' BLAST_1st_best_hit.txt > BLAST_1st_best_hit_tmp && mv BLAST_1st_best_hit_tmp BLAST_1st_best_hit.txt
+
+# Add sim_score columns to BLAST_10_best_hits.txt for each hit block
+awk -F'+' 'BEGIN{OFS="+"}
+NR==1 {
+   print $0, "sim_score_hit1","sim_score_hit2","sim_score_hit3","sim_score_hit4","sim_score_hit5","sim_score_hit6","sim_score_hit7","sim_score_hit8","sim_score_hit9","sim_score_hit10"; next}
+{
+   for(i=1; i<=10; i++){
+       start = 1 + (i-1)*17;
+       qlen = $(start+2);
+       align_len = $(start+9);
+       pident = $(start+16);
+       if(qlen+0 > 0) {
+           sim = pident * (align_len / qlen);
+       } else {
+           sim = "NA";
+       }
+       sim_scores[i] = sim;
+   }
+   print $0, sim_scores[1], sim_scores[2], sim_scores[3], sim_scores[4], sim_scores[5], sim_scores[6], sim_scores[7], sim_scores[8], sim_scores[9], sim_scores[10]
+}' BLAST_10_best_hits.txt > BLAST_10_best_hits_tmp && mv BLAST_10_best_hits_tmp BLAST_10_best_hits.txt
 
 #################################################
 ### COMPILE FINAL STATISTICS AND README FILES ###
@@ -158,12 +179,12 @@ sed -i 's/\t/+/g' BLAST_10_best_hits.txt
 printf "\nCleaning up and compiling final stats files ...\n"
 
 if [[ $debugger != "true" ]]; then
-	if [[ -d tempdir ]]; then
-		rm -r tempdir
-	fi
-	if [[ -d tempdir2 ]]; then
-		rm -r tempdir2
-	fi
+    if [[ -d tempdir ]];then
+        rm -r tempdir
+    fi
+    if [[ -d tempdir2 ]];then
+        rm -r tempdir2
+    fi
 fi
 
 end=$(date +%s)
@@ -173,7 +194,7 @@ runtime=$((end-start))
 db_x=$(echo $db1 | sed -e 's/\/extraFiles\///')
 printf "# Taxonomy was assigned using BLAST (see 'Core command' below for the used settings).
 
-Query    = $fasta_file	
+Query    = $IN
 Database = $db_x
 
 BLAST_1st_best_hit.txt = BLAST results for the 1st best hit in the used database.
@@ -199,15 +220,15 @@ gaps      = total number of gaps
 sstrand   = subject strand
 qcovs     = query coverage per subject
 pident    = percentage of identical matches
-sim_score = similarity score calculated as (pident * (alignment length/qlen))
+sim_score = similarity score calculated as (pident*(alignment length/qlen))
 
 Core command -> 
-blastn -query $fasta_file $strands $database $task -max_target_seqs 10 $evalue $wordsize $reward $penalty $gapopen $gapextend -max_hsps 1
+blastn -query $IN $strands $database $task -max_target_seqs 10 $evalue $wordsize $reward $penalty $gapopen $gapextend -max_hsps 1
 
 Total run time was $runtime sec.
 
-################################################
-###Third-party applications:
+##########################################################
+###Third-party applications [PLEASE CITE]:
     #BLAST 2.14.0+
     #citation: Camacho C., Coulouris G., Avagyan V., Ma N., Papadopoulos J., Bealer K., & Madden T.L. (2008) BLAST+: architecture and applications. BMC Bioinformatics 10:421. 
 #####################################################" > $output_dir/README.txt
