9687217d967e442b5545f7d950a7643e3f7b5340
Martin Metsoja <martin.metsoja@ut.ee>
2025-04-22 09:59:37 +0300

    minor design changes

--- src/background.js
+++ src/background.js
@@ -95,9 +95,9 @@ async function createWindow() {
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
-      enableRemoteModule: true,
     },
   });
+  remoteMain.enable(win.webContents);
   win.removeMenu();
     // Add keyboard shortcut for DevTools
   win.webContents.on('before-input-event', (event, input) => {
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,13 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: '#1DE9B6', // Your desired primary color
+      }
+    }
+  }
+});
